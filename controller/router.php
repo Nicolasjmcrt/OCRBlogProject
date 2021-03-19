8ed2fb1040198fdc87c3bb22e93f26cf1f4faa42
Nicolas <jumeaucourtn@gmail.com>
2021-03-19 18:04:12 +0100

    Modifs CSS et router (formattage)

--- controller/router.php
+++ controller/router.php
@@ -2,39 +2,33 @@
 
 class router
 {
-	private $url;
-	public function __construct($url){
-		$this->url=$url;
-	}
+    private $url;
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
 
-	public function run()
-	{
-		$data = explode('/',$this->url);
-	
-		if (! isset($data[0]) || empty ($data[0])) {
-			$controller = 'homeController';
-		} else {
-			$controller = $data[0].'Controller';
-		}
-		
-		if (! isset($data[1]) || empty ($data[1])) {
-			$method = 'list';
-		}
-		else {
-		$method = $data[1];
-		}
-		$params = null;
-		if (isset($data[2])) {
-			$params = $data[2];
-		}
-		require_once 'controller/'.$controller.'.php';
-		$ctrl = new $controller;
-		$ctrl->$method($params);
-	}
+    public function run()
+    {
+        $data = explode('/', $this->url);
+
+        if (!isset($data[0]) || empty($data[0])) {
+            $controller = 'homeController';
+        } else {
+            $controller = $data[0] . 'Controller';
+        }
+
+        if (!isset($data[1]) || empty($data[1])) {
+            $method = 'list';
+        } else {
+            $method = $data[1];
+        }
+        $params = null;
+        if (isset($data[2])) {
+            $params = $data[2];
+        }
+        require_once 'controller/' . $controller . '.php';
+        $ctrl = new $controller;
+        $ctrl->$method($params);
+    }
 }
-
-
-
-
-
- ?>
